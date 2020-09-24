6d4dab1170c78fef0d9fee9205b9083d68d95d43
Arya Kulkarni <kulkarya@gmail.com>
2020-09-24 18:08:41 -0400

    hi

--- src/TransactionPkg/TransactionManager.java
+++ src/TransactionPkg/TransactionManager.java
@@ -24,19 +24,10 @@ public class TransactionManager {
                 }else if(command.length() == 2){
                     System.out.println("Okay");
                 }
-
-                if (command == "1")
-                {
-                    throw new Exception();
-                }
             }catch(InputMismatchException e){
                 System.out.println(e.getMessage());
-                System.out.println("Invalid Input");
             }
-            catch(Exception e)
-            {
-                System.out.println(e.getMessage());
-            }
+
 
         }while(sc.hasNext()) ;
     }
