6bb89fcaa138c909b197b237b61466236fa961c1
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-10-08 20:40:52 -0400

    final push

--- src/TransactionPkg/MoneyMarketTest.java
+++ src/TransactionPkg/MoneyMarketTest.java
@@ -6,23 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyMarketTest {
 
-    @Test
-    void countWithdrawals() {
-    }
+    Account a = new MoneyMarket(new Profile("Jesse", "Barbieri"),500, new Date(2010,1,1)),
+            b = new MoneyMarket(new Profile("Arya", "Kulkarni"),1000, new Date(2010,1,1)),
+            c = new MoneyMarket(new Profile("Thomas", "Jefferson"),2500, new Date(2013,3,27)),
+            d = new MoneyMarket(new Profile("Jesse", "Barbieri"),2500, new Date(2010,1,1)),
+            e = new Savings(new Profile("Jesse", "Barbieri"),500, new Date(2010,1,1),true);
+
 
     @Test
     void testEquals() {
+        assertTrue(a.equals(d), "Equal names should return true");
+        assertFalse(a.equals(e), "Should return false, different account types, same name");
     }
 
     @Test
     void testToString() {
+        assertEquals("*Money Market*Arya Kulkarni* $1,000.00*1/1/2010*0 withdrawals*", b.toString());
     }
 
     @Test
     void monthlyInterest() {
+        assertEquals((0.0065/12.00), a.monthlyInterest(), "All interest should be equal");
+        assertEquals((0.0065/12.00), b.monthlyInterest(), "All interest should be equal");
+        assertEquals((0.0065/12.00), c.monthlyInterest(), "All interest should be equal");
     }
 
     @Test
     void monthlyFee() {
+        assertEquals(0, c.monthlyFee());
+        assertEquals(0, d.monthlyFee());
     }
 }
