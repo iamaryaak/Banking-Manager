b2dc1d2162f89f4e1bc930072d64966ef4384491
Arya Kulkarni <kulkarya@gmail.com>
2020-10-26 15:14:14 -0400

    stuff

--- src/TransactionPkg/Controller.java
+++ src/TransactionPkg/Controller.java
@@ -1,16 +1,34 @@
 package TransactionPkg;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class Controller {
 
     public TextField firstName;
+    public TextField lastName;
+    public TextField month;
+    public TextField day;
+    public TextField year;
+    public TextField balance;
+
     public Button openAccount;
 
-    public void sayFirstName(ActionEvent actionEvent) {
+    public RadioButton checking;
+    public RadioButton savings;
+    public RadioButton moneyMarket;
+
+    public void sayName(ActionEvent actionEvent) {
         String n = firstName.getText();
-        System.out.println("Got name " + n);
+        String l = lastName.getText();
+
+        System.out.println("Got name: " + n + " " + l);
+    }
+
+    public void checkingSelect(ActionEvent e){
+        System.out.println("Checking Account Selected");
+        // gray out other 2
     }
 
 }
