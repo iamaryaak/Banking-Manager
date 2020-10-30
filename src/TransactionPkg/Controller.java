e23c79f11c410a34d52f4029da95bb253062ebe7
Arya Kulkarni <kulkarya@gmail.com>
2020-10-30 03:23:46 -0400

    Error created for invalid date

--- src/TransactionPkg/Controller.java
+++ src/TransactionPkg/Controller.java
@@ -2,12 +2,8 @@ package TransactionPkg;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -358,6 +354,7 @@ public class Controller {
                 }
             } else {
                 System.out.println(dateOpen.toString() + " is not a valid date!");
+                display(dateOpen);
             }
 
         } else if (savings.isSelected()) {
@@ -379,6 +376,7 @@ public class Controller {
                 }
             } else {
                 System.out.println(dateOpen.toString() + " is not a valid date!");
+                display(dateOpen);
             }
 
         } else if (moneyMarket.isSelected()) { // money Market is selected
@@ -401,6 +399,7 @@ public class Controller {
                 }
             } else {
                 System.out.println(dateOpen.toString() + " is not a valid date!");
+                display(dateOpen);
             }
         }
 
@@ -690,34 +689,11 @@ public class Controller {
 
 
     // make popup window for not importing file
-    public static void display() {
-        Stage popupwindow = new Stage();
-
-        popupwindow.initModality(Modality.APPLICATION_MODAL);
-        popupwindow.setTitle("This is a pop up window");
-
-
-        Label label1 = new Label("Pop up window now displayed");
-
-
-        Button button1 = new Button("Close this pop up window");
-
-
-        button1.setOnAction(e -> popupwindow.close());
-
-
-        VBox layout = new VBox(10);
-
-
-        layout.getChildren().addAll(label1, button1);
-
-        layout.setAlignment(Pos.CENTER);
-
-        Scene scene1 = new Scene(layout, 300, 250);
-
-        popupwindow.setScene(scene1);
-
-        popupwindow.showAndWait();
+    public static void display(Date date) {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Input not valid");
+        errorAlert.setContentText(date.toString() + " is not a valid date. Please enter the correct date");
+        errorAlert.showAndWait();
 
     }
 }
